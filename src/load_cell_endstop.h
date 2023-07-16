8e85a6471bef3b0d7e06700266b645a0391602e1
Gareth Farrington <gareth@waves.ky>
2023-07-15 22:00:30 -0700

    Delete comments

--- src/load_cell_endstop.h
+++ src/load_cell_endstop.h
@@ -6,10 +6,5 @@
 struct load_cell_endstop *load_cell_endstop_oid_lookup(uint8_t oid);
 void load_cell_endstop_report_sample(struct load_cell_endstop *lce
                         , int32_t sample, uint32_t ticks);
-/*
-void load_cell_endstop_report_error(struct load_cell_endstop *lce
-                        , uint8_t error_code);
-void load_cell_endstop_source_stopped(struct load_cell_endstop *lce);
-*/
 
 #endif // load_cell_endstop.h
